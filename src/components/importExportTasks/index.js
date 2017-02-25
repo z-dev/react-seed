ac4437d856a4cdb003348f32db13ad2d248ec93f
Richard Gill <richard@rgill.co.uk>
2017-02-25 16:55:24 +0000

    Added import Export tasks

---
+++ src/components/importExportTasks/index.js
@@ -0,0 +1,10 @@
+import React from 'react' //ignore this
+
+// Answers go here!
+
+
+
+
+
+// Ignore this!
+export default () => <p>Import Export Tasks. Open your debugger :)</p>
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import Route from './components/app/route'
 
 import Landing from './components/landing'
 import ReactTasks from './components/reactTasks'
+import ImportExportTasks from './components/importExportTasks'
 
 export const routes = [
   { path: 'reactTasks', component: ReactTasks },
+  { path: 'importExportTasks', component: ImportExportTasks },
 ]
 
 export default {
