005cbc34ff412d46a02b05abfc61e5a5503aabef
Richard Gill <richard@rgill.co.uk>
2017-03-15 14:10:06 +0000

    made code more readible

--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -8,7 +8,10 @@ export default (props) =>
     {_.map(props.todos, (todo) => <Todo todo={todo} key={todo.id} onTodoDeleted={props.onTodoDeleted} onTodoCompleted={props.onTodoCompleted} />)}
     <p>Add a Todo</p>
     <Form onSubmit={() => props.onTodoAdded()}>
-      <input onChange={(e) => props.onUpdateTodoText(e.target.value)} value={props.currentlyEditingTodoText} />
+      <input
+        onChange={(e) => props.onUpdateTodoText(e.target.value)}
+        value={props.currentlyEditingTodoText}
+      />
       <button>Add</button>
     </Form>
   </div>
--- src/redux/actions/todos.js
+++ src/redux/actions/todos.js
@@ -1,7 +1,15 @@
-export const addTodo = (todo) => ({ type: 'todos/ADD_TODO', payload: todo })
+export const addTodo = (todo) => {
+  return {type: 'todos/ADD_TODO', payload: todo }
+}
 
-export const updateTodoText = (todoText) => ({ type: 'todos/UPDATE_TODO_TEXT', payload: todoText })
+export const updateTodoText = (todoText) => {
+  return { type: 'todos/UPDATE_TODO_TEXT', payload: todoText }
+}
 
-export const completeTodo = (todoId) => ({ type: 'todos/COMPLETE_TODO', payload: todoId })
+export const completeTodo = (todoId) => {
+  return { type: 'todos/COMPLETE_TODO', payload: todoId }
+}
 
-export const deleteTodo = (todoId) => ({ type: 'todos/DELETE_TODO', payload: todoId })
+export const deleteTodo = (todoId) => {
+  return { type: 'todos/DELETE_TODO', payload: todoId }
+}
